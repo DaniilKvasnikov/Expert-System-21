a6d57ac667b65c073fba464a8670f52b0d9b6645
rrhaenys <123yk123@mail.ru>
2020-03-06 10:44:27 +0300

    refactor log

--- Expert-System-21/ExpertSystem/Log/Log.cs
+++ Expert-System-21/ExpertSystem/Log/Log.cs
@@ -8,9 +8,7 @@ namespace ExpertSystemTests.ExpertSystem.Log
         public static void PrintResults(Dictionary<char, bool?> results, bool check)
         {
             foreach (var result in results)
-            {
                 Console.WriteLine(result.Key + " : " + result.Value);
-            }
             Console.ForegroundColor = check ? ConsoleColor.Green : ConsoleColor.Red;
             Console.WriteLine(check + ". The end!");
             Console.ResetColor();;
