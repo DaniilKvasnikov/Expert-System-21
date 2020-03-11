400ef58bbf42f8474ad74c1124834b5a4849530f
gamerd <daniil.kvasnikov@gmail.com>
2020-03-11 11:10:36 +0300

    Logger.cs

--- Expert-System-21/Expert_System_21/Logs/Logger.cs
+++ Expert-System-21/Expert_System_21/Logs/Logger.cs
@@ -7,14 +7,14 @@ namespace Expert_System_21.Logs
     public static class Logger
     {
         public static bool Log;
+
         public static void PrintResults(Dictionary<char, bool?> results, bool check)
         {
             Log = true;
             foreach (var result in results)
                 LogString(result.Key + " : " + result.Value);
-            Console.ForegroundColor = check ? ConsoleColor.Green : ConsoleColor.Red;
-            LogString(check + ". The end!");
-            Console.ResetColor();
+            var color = check ? ConsoleColor.Green : ConsoleColor.Red;
+            LogString(check + ". The end!", color: color);
             Log = false;
         }
 
@@ -23,14 +23,14 @@ namespace Expert_System_21.Logs
             LogString(atom + " init " + atom.State);
         }
 
-        public static void LogString(string message, int tabCount = 0)
+        public static void LogString(string message, int tabCount = 0, ConsoleColor? color = null)
         {
             if (!Log) return;
-            while (--tabCount >= 0)
-            {
-                Console.Write('\t');
-            }
+            while (--tabCount >= 0) Console.Write('\t');
+            if (color != null)
+                Console.ForegroundColor = color.Value;
             Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
